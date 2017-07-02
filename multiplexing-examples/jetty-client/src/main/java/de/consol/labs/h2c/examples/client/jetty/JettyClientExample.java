c75712373e69e673df59ebf3ca712bfefcdd9d76
Jeremy Li <lizy39@lenovo.com>
2017-07-02 21:20:00 +0800

    log4j added.

--- multiplexing-examples/jetty-client/src/main/java/de/consol/labs/h2c/examples/client/jetty/JettyClientExample.java
+++ multiplexing-examples/jetty-client/src/main/java/de/consol/labs/h2c/examples/client/jetty/JettyClientExample.java
@@ -22,53 +22,54 @@ import java.util.concurrent.TimeUnit;
 // Copied from the JavaDoc for org.eclipse.jetty.http2.client.HTTP2Client
 public class JettyClientExample {
 
-    public static void main(String[] args) throws Exception {
-        long startTime = System.nanoTime();
+	public static void main(String[] args) throws Exception {
+		long startTime = System.nanoTime();
 
-        // Create and start HTTP2Client.
-        HTTP2Client client = new HTTP2Client();
-        SslContextFactory sslContextFactory = new SslContextFactory(true);
-        client.addBean(sslContextFactory);
-        client.start();
+		// Create and start HTTP2Client.
+		HTTP2Client client = new HTTP2Client();
+		SslContextFactory sslContextFactory = new SslContextFactory(true);
+		client.addBean(sslContextFactory);
+		client.start();
 
-        // Connect to host.
-        String host = "localhost";
-        int port = 8443;
+		// Connect to host.
+		String host = "localhost";
+		int port = 8443;
 
-        FuturePromise<Session> sessionPromise = new FuturePromise<>();
-        client.connect(sslContextFactory, new InetSocketAddress(host, port), new ServerSessionListener.Adapter(), sessionPromise);
+		FuturePromise<Session> sessionPromise = new FuturePromise<>();
+		client.connect(sslContextFactory, new InetSocketAddress(host, port), new ServerSessionListener.Adapter(),
+				sessionPromise);
 
-        // Obtain the client Session object.
-        Session session = sessionPromise.get(5, TimeUnit.SECONDS);
+		// Obtain the client Session object.
+		Session session = sessionPromise.get(5, TimeUnit.SECONDS);
 
-        // Prepare the HTTP request headers.
-        HttpFields requestFields = new HttpFields();
-        requestFields.put("User-Agent", client.getClass().getName() + "/" + Jetty.VERSION);
-        // Prepare the HTTP request object.
-        MetaData.Request request = new MetaData.Request("GET", new HttpURI("https://" + host + ":" + port + "/"), HttpVersion.HTTP_2, requestFields);
-        // Create the HTTP/2 HEADERS frame representing the HTTP request.
-        HeadersFrame headersFrame = new HeadersFrame(request, null, true);
+		// Prepare the HTTP request headers.
+		HttpFields requestFields = new HttpFields();
+		requestFields.put("User-Agent", client.getClass().getName() + "/" + Jetty.VERSION);
+		// Prepare the HTTP request object.
+		MetaData.Request request = new MetaData.Request("GET", new HttpURI("https://" + host + ":" + port + "/"),
+				HttpVersion.HTTP_2, requestFields);
+		// Create the HTTP/2 HEADERS frame representing the HTTP request.
+		HeadersFrame headersFrame = new HeadersFrame(request, null, true);
 
-        // Prepare the listener to receive the HTTP response frames.
-        Stream.Listener responseListener = new Stream.Listener.Adapter()
-        {
-            @Override
-            public void onData(Stream stream, DataFrame frame, Callback callback)
-            {
-                byte[] bytes = new byte[frame.getData().remaining()];
-                frame.getData().get(bytes);
-                int duration = (int) TimeUnit.NANOSECONDS.toSeconds(System.nanoTime() - startTime);
-                System.out.println("After " + duration + " seconds: " + new String(bytes));
-                callback.succeeded();
-            }
-        };
+		// Prepare the listener to receive the HTTP response frames.
+		Stream.Listener responseListener = new Stream.Listener.Adapter() {
+			@Override
+			public void onData(Stream stream, DataFrame frame, Callback callback) {
+				byte[] bytes = new byte[frame.getData().remaining()];
+				frame.getData().get(bytes);
+				int duration = (int) TimeUnit.NANOSECONDS.toSeconds(System.nanoTime() - startTime);
+				System.out.println("After " + duration + " seconds, received length: " + bytes.length + " data:"
+						+ new String(bytes));
+				callback.succeeded();
+			}
+		};
 
-        session.newStream(headersFrame, new FuturePromise<>(), responseListener);
-        session.newStream(headersFrame, new FuturePromise<>(), responseListener);
-        session.newStream(headersFrame, new FuturePromise<>(), responseListener);
+		for (int i = 0; i < 100; i++) {
+			session.newStream(headersFrame, new FuturePromise<>(), responseListener);
+		}
 
-        Thread.sleep(TimeUnit.SECONDS.toMillis(20));
+		Thread.sleep(TimeUnit.SECONDS.toMillis(20));
 
-        client.stop();
-    }
+		client.stop();
+	}
 }
--- multiplexing-examples/server/src/main/java/de/consol/labs/h2c/examples/server/Servlet.java
+++ multiplexing-examples/server/src/main/java/de/consol/labs/h2c/examples/server/Servlet.java
@@ -1,31 +1,42 @@
 package de.consol.labs.h2c.examples.server;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class Servlet extends HttpServlet {
 
-    private static final int DELAY_IN_SECONDS = 6;
-    private final ConnectionTracker connectionTracker = ConnectionTracker.getInstance();
-    private final AtomicInteger numberOfRequests = new AtomicInteger(0);
+	/**
+	 * 
+	 */
+	private static final Logger logger = LogManager.getLogger(Servlet.class.getName());
 
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        int numberOfCurrentRequest = numberOfRequests.incrementAndGet();
-        int numberOfCurrentConnection = connectionTracker.getNumberOfConnections();
-        try {
-            Thread.sleep(TimeUnit.SECONDS.toMillis(DELAY_IN_SECONDS));
-        } catch (InterruptedException e) {
-            throw new ServletException(e);
-        }
-        resp.setContentType("text/plain");
-        resp.getWriter().write("This is the response for request number " + numberOfCurrentRequest +
-                " using connection number " + numberOfCurrentConnection +
-                " after " + DELAY_IN_SECONDS + " seconds delay.");
-    }
+	private static final long serialVersionUID = -6243985929484239357L;
+	private static final int DELAY_IN_SECONDS = 6;
+	private final ConnectionTracker connectionTracker = ConnectionTracker.getInstance();
+	private final AtomicInteger numberOfRequests = new AtomicInteger(0);
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		logger.info("req:"+req.getCookies());
+		int numberOfCurrentRequest = numberOfRequests.incrementAndGet();
+		int numberOfCurrentConnection = connectionTracker.getNumberOfConnections();
+		try {
+			Thread.sleep(TimeUnit.SECONDS.toMillis(DELAY_IN_SECONDS));
+		} catch (InterruptedException e) {
+			throw new ServletException(e);
+		}
+		resp.setContentType("text/plain");
+		resp.getWriter()
+				.write("This is the response for request number " + numberOfCurrentRequest + " using connection number "
+						+ numberOfCurrentConnection + " after " + DELAY_IN_SECONDS + " seconds delay.");
+	}
 }
